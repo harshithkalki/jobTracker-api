b44f04e0d72d20e5a7982c59510b35f029afede7
harshith reddy chiluka <harshithkalki@gmail.com>
2026-03-22 21:34:23 -0400

    Changes -- added docker, added dependencies and db connection

---
+++ src/server.ts
@@ -0,0 +1,18 @@
+import app from './app';
+import dotenv from 'dotenv';
+import pool from './db/pool';
+
+dotenv.config();
+
+const PORT=process.env.PORT || 3000;
+
+pool.connect().then(client=>{
+    console.log('Connected to the database');
+}).catch(err=>{
+    console.error('Error connecting to the database',err);
+    process.exit(1);
+}) 
+
+app.listen(PORT,()=>{
+    console.log(`Server is running on port ${PORT}`);
+})
