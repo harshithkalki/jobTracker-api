95b3bf7babdfbb5fbf38075b8ff004031861d815
harshith reddy chiluka <harshithkalki@gmail.com>
2026-04-02 17:42:41 -0400

    AWS S3 config

---
+++ src/controllers/documentController.ts
@@ -0,0 +1,66 @@
+import {Request, Response, NextFunction} from 'express';
+import {PutObjectCommand, DeleteObjectCommand } from '@aws-sdk/client-s3';
+import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
+import dotenv from "dotenv";
+import s3Client from '../config/s3';
+import pool from '../db/pool';
+
+dotenv.config()
+
+export const getPresignedUrl = async (req:Request, res:Response,next: NextFunction) =>{
+    try{
+        const jobId = req.params.jobId;
+        const {filename, filetype} = req.body;
+        const key = `uploads/${jobId}/${Date.now()}-${filename}`;
+        const commaand = new PutObjectCommand({
+            Bucket: process.env.AWS_S3_BUCKET,
+            Key: key,
+            ContentType:filetype
+        });
+        const url= await getSignedUrl(s3Client,commaand,{expiresIn:600});
+        res.json({url, key});
+
+    }catch(error){
+        next(error);
+    }
+}
+
+export const saveDocument = async (req:Request, res:Response, next: NextFunction) =>{
+try{
+    const {jobId} = req.params;
+    const {filename, filetype, s3_key}= req.body;
+    const result = await pool.query('insert into documents (job_id, file_name, file_type, s3_key) values ($1, $2, $3, $4) returning *', [jobId, filename, filetype, s3_key]);
+    res.status(201).json({message: 'Document saved successfully', result: result.rows[0]});
+}catch(error){
+    next(error);   
+}
+}
+
+export const getDocuments = async (req:Request, res:Response, next: NextFunction) =>{
+    try{
+        const jobId = req.params.jobId;
+        const result = await pool.query('select * from documents where job_id = $1', [jobId]);
+        res.json(result.rows);
+    }catch(error){
+        next(error);
+    }
+}
+
+export const deleteDocument = async (req:Request, res:Response, next: NextFunction) =>{
+    try{
+        const documentId = req.params.documentId;
+        const result = await pool.query('select * from documents where document_id = $1', [documentId]);
+        if(result.rowCount === 0){
+            return res.status(404).json({message: 'Document not found'});
+        }
+        const s3Key = result.rows[0].s3_key;
+        await s3Client.send(new DeleteObjectCommand({
+            Bucket: process.env.AWS_S3_BUCKET,
+            Key: s3Key
+        }));
+        await pool.query('DELETE FROM documents WHERE document_id = $1', [documentId]);
+        res.json({message: 'Document deleted successfully', result: result.rows[0]});
+    }catch(error){
+        next(error);
+    }
+}
